def7f5fbe9393102e4090f08209447ba20acff22
desarrollo-siscadat <desarrollo.siscadat@gmail.com>
2021-03-22 10:37:32 -0300

    drivers fbtft otra vez
    Signed-off-by: desarrollo-siscadat <desarrollo.siscadat@gmail.com>

--- drivers/video/fbdev/fbtft/fbtft-core.c
+++ drivers/video/fbdev/fbtft/fbtft-core.c
@@ -422,7 +422,7 @@ void fbtft_update_display(struct fbtft_par *par, unsigned start_line, unsigned e
 			par->update_time.tv_sec = ts_start.tv_sec;
 			par->update_time.tv_nsec = ts_start.tv_nsec;
 		}
-		ts_fps = timespec_sub(ts_start, par->update_time);
+		ts_fps = timespec64_sub(ts_start, par->update_time);
 		par->update_time.tv_sec = ts_start.tv_sec;
 		par->update_time.tv_nsec = ts_start.tv_nsec;
 		fps_ms = (ts_fps.tv_sec * 1000) + ((ts_fps.tv_nsec / 1000000) % 1000);
@@ -430,7 +430,7 @@ void fbtft_update_display(struct fbtft_par *par, unsigned start_line, unsigned e
 		fps = fps_ms * 1000 + fps_us;
 		fps = fps ? 1000000 / fps : 0;
 
-		ts_duration = timespec_sub(ts_end, ts_start);
+		ts_duration = timespec64_sub(ts_end, ts_start);
 		duration_ms = (ts_duration.tv_sec * 1000) + ((ts_duration.tv_nsec / 1000000) % 1000);
 		duration_us = (ts_duration.tv_nsec / 1000) % 1000;
 		throughput = duration_ms * 1000 + duration_us;
